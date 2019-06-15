b82b5f16b09e81d9645750ed5a1265f50b78c5f9
tuanama13 <tuanama7@gmail.com>
2019-06-16 01:07:16 +0700

    add database

--- botanical.sql
+++ botanical.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 04, 2019 at 05:34 AM
+-- Generation Time: Jun 15, 2019 at 08:06 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -32,6 +32,7 @@ CREATE TABLE `tbl_cabang` (
   `id_cabang` int(11) NOT NULL,
   `nama_cabang` varchar(100) NOT NULL,
   `alamat_cabang` text NOT NULL,
+  `jumlah_meja` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `logs` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -40,8 +41,8 @@ CREATE TABLE `tbl_cabang` (
 -- Dumping data for table `tbl_cabang`
 --
 
-INSERT INTO `tbl_cabang` (`id_cabang`, `nama_cabang`, `alamat_cabang`, `id_user`, `logs`) VALUES
-(1, 'Botanical dr. Wahidin', 'Jl. dr. Wahidin .S. No 88', 1, 1);
+INSERT INTO `tbl_cabang` (`id_cabang`, `nama_cabang`, `alamat_cabang`, `jumlah_meja`, `id_user`, `logs`) VALUES
+(1, 'Botanical dr. Wahidin', 'Jl. dr. Wahidin .S. No 88', 10, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -56,6 +57,27 @@ CREATE TABLE `tbl_detail_order` (
   `jumlah_order` int(11) NOT NULL,
   `harga_produk` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_detail_order`
+--
+
+INSERT INTO `tbl_detail_order` (`id_detail_order`, `id_order`, `id_produk`, `jumlah_order`, `harga_produk`) VALUES
+(1, 1, 7, 1, 24000),
+(2, 1, 5, 1, 15500),
+(3, 1, 2, 2, 5000),
+(4, 2, 2, 1, 5000),
+(5, 3, 8, 1, 25000),
+(6, 4, 1, 1, 15000),
+(7, 5, 8, 1, 25000),
+(8, 6, 2, 2, 5000),
+(9, 6, 5, 1, 15500),
+(10, 6, 7, 1, 24000),
+(11, 7, 2, 4, 5000),
+(12, 7, 8, 1, 25000),
+(13, 7, 7, 1, 24000),
+(14, 7, 6, 3, 25500),
+(15, 7, 5, 1, 15500);
 
 -- --------------------------------------------------------
 
@@ -76,7 +98,8 @@ CREATE TABLE `tbl_kategori` (
 
 INSERT INTO `tbl_kategori` (`id_kat`, `nama_kat`, `desc_kat`, `logs`) VALUES
 (1, 'Makanan', 'This our database connection function. I prefixed it with db_, which I will do as well for the rest of the database functions later. If we were using OOP, we could have an even cleaner looking interface by creating a database class which has the operations as the method names (connect(), query(), etc.). An OOP version of this tutorial has been included at the end of the tutorial.', 1),
-(2, 'Minuman', 'This our database connection function. I prefixed it with db_, which I will do as well for the rest of the database functions later. If we were using OOP, we could have an even cleaner looking interface by creating a database class which has the operations as the method names (connect(), query(), etc.). An OOP version of this tutorial has been included at the end of the tutorial.', 1);
+(2, 'Minuman', 'This our database connection function. I prefixed it with db_, which I will do as well for the rest of the database functions later. If we were using OOP, we could have an even cleaner looking interface by creating a database class which has the operations as the method names (connect(), query(), etc.). An OOP version of this tutorial has been included at the end of the tutorial.', 1),
+(3, 'Snack', 'none', 1);
 
 -- --------------------------------------------------------
 
@@ -106,12 +129,26 @@ INSERT INTO `tbl_logs` (`id_logs`, `id_user`, `tgl_logs`, `ket_logs`) VALUES
 
 CREATE TABLE `tbl_orders` (
   `id_order` int(11) NOT NULL,
+  `no_meja` int(11) NOT NULL,
   `id_cabang` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `tgl_order` datetime NOT NULL,
+  `tgl_order` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `grandtotal` double NOT NULL,
   `logs` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_orders`
+--
+
+INSERT INTO `tbl_orders` (`id_order`, `no_meja`, `id_cabang`, `id_user`, `tgl_order`, `grandtotal`, `logs`) VALUES
+(1, 2, 1, 1, '2019-06-11 12:48:49', 49500, 1),
+(2, 6, 1, 1, '2019-06-11 12:50:36', 5000, 1),
+(3, 3, 1, 1, '2019-06-11 13:08:08', 25000, 1),
+(4, 8, 1, 1, '2019-06-11 13:10:01', 15000, 1),
+(5, 3, 1, 1, '2019-06-14 13:35:11', 25000, 1),
+(6, 5, 1, 1, '2019-06-14 13:43:56', 49500, 1),
+(7, 7, 1, 1, '2019-06-14 16:03:01', 161000, 1);
 
 -- --------------------------------------------------------
 
@@ -152,6 +189,14 @@ CREATE TABLE `tbl_pengeluaran` (
   `logs` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tbl_pengeluaran`
+--
+
+INSERT INTO `tbl_pengeluaran` (`id_pengeluaran`, `id_user`, `tgl_pengeluaran`, `ket_pengeluaran`, `jumlah_pengeluaran`, `logs`) VALUES
+(1, 1, '2019-06-14 00:00:00', 'Indomie 1 Dus', 120000, 1),
+(2, 1, '2019-06-14 00:00:00', 'Bensin 2 Liter', 15000, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -174,8 +219,8 @@ CREATE TABLE `tbl_produk` (
 --
 
 INSERT INTO `tbl_produk` (`id_produk`, `id_kat`, `nama_produk`, `desc_produk`, `image_produk`, `harga_produk`, `status_produk`, `logs`) VALUES
-(1, 1, 'Mac and Cheese', 'This diagram shows Address Types, which are an example of Reference Data.\r\nThis kind of data has the following characteristics :-\r\n\r\n    it doesn\'t change very much\r\n    it has a relatively small number of values, usually less than a few dozen and never more than a few hundred.\r\n    Therefore we can show it with a Code as a Primary Key.\r\n    Data in Reference Data Tables can be used to populate drop-down lists for Users to select from.\r\n    In this way, it is used to ensure that all new data is valid. \r\n\r\n', '/image/1/1.jpg', 15000, 0, 1),
-(2, 2, 'Ice Tea', 'This diagram shows Address Types, which are an example of Reference Data.\r\nThis kind of data has the following characteristics :-\r\n\r\n    it doesn\'t change very much\r\n    it has a relatively small number of values, usually less than a few dozen and never more than a few hundred.\r\n    Therefore we can show it with a Code as a Primary Key.\r\n    Data in Reference Data Tables can be used to populate drop-down lists for Users to select from.\r\n    In this way, it is used to ensure that all new data is valid. \r\n\r\n', '/image/2/1.jpg', 5000, 0, 1),
+(1, 1, 'Mac and Cheese', 'This diagram shows Address Types, which are an example of Reference Data.\r\nThis kind of data has the following characteristics :-\r\n\r\n    it doesn\'t change very much\r\n    it has a relatively small number of values, usually less than a few dozen and never more than a few hundred.\r\n    Therefore we can show it with a Code as a Primary Key.\r\n    Data in Reference Data Tables can be used to populate drop-down lists for Users to select from.\r\n    In this way, it is used to ensure that all new data is valid. \r\n\r\n', '/image/1/1.jpg', 15000, 1, 1),
+(2, 2, 'Ice Tea', 'This diagram shows Address Types, which are an example of Reference Data.\r\nThis kind of data has the following characteristics :-\r\n\r\n    it doesn\'t change very much\r\n    it has a relatively small number of values, usually less than a few dozen and never more than a few hundred.\r\n    Therefore we can show it with a Code as a Primary Key.\r\n    Data in Reference Data Tables can be used to populate drop-down lists for Users to select from.\r\n    In this way, it is used to ensure that all new data is valid. \r\n\r\n', '/image/2/1.jpg', 5000, 1, 1),
 (5, 1, 'Indomie Gokil', 'Semangkuk Indomie-nya hadir dengan daging kikil dan sambal cabe hijau-nya. Sensasi rasa kenyal pada kikil-nya, serta Indomie-nya yang gurih dan pedas memang nagih banget. Mantap! ', '/image/1/2.jpg', 15500, 1, 1),
 (6, 1, 'Nasi Wagyu Slice Saus Mushroom', 'Yang satu ini juga nggak boleh ketinggalan buat kamu cicipi. Ngelihat fotonya saja sudah bisa bikin ngiler. Nasi putih hangat dengan irisan daging wagyu dengan saus mushroom-nya yang gurih, creamy, dan manis juara banget memang rasanya. Nggak ketinggalan telur mata sapinya. Hmmm, siap-siap di bikin jatuh cinta ya sama kelezatannya! ', '/image/1/3.jpg', 25500, 1, 1),
 (7, 1, 'Cireng Pandawa', 'Tekstur garing dan crunchy pada bagian luarnya, serta dalamnya yang kenyal dan rasanya yang gurih memang nggak bakalan bisa di tolak. Apalagi kalau dicocol dengan sambalnya yang pedas manis', '/image/1/4.jpg', 24000, 1, 1),
@@ -277,13 +322,13 @@ ALTER TABLE `tbl_cabang`
 -- AUTO_INCREMENT for table `tbl_detail_order`
 --
 ALTER TABLE `tbl_detail_order`
-  MODIFY `id_detail_order` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detail_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tbl_kategori`
 --
 ALTER TABLE `tbl_kategori`
-  MODIFY `id_kat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_kat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_logs`
@@ -295,7 +340,7 @@ ALTER TABLE `tbl_logs`
 -- AUTO_INCREMENT for table `tbl_orders`
 --
 ALTER TABLE `tbl_orders`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tbl_pegawai`
@@ -307,7 +352,7 @@ ALTER TABLE `tbl_pegawai`
 -- AUTO_INCREMENT for table `tbl_pengeluaran`
 --
 ALTER TABLE `tbl_pengeluaran`
-  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_produk`
